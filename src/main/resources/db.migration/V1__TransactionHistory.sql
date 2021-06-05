3f1066c8583133009e5d537c9b176d4ff624862d
Lavanya C <lavanya_c@gap.com>
2021-06-05 11:54:14 +0530

    Update schema

--- src/main/resources/db.migration/V1__TransactionHistory.sql
+++ src/main/resources/db.migration/V1__TransactionHistory.sql
@@ -1,19 +1,19 @@
 CREATE TABLE transaction_detail (
 	id VARCHAR(256) PRIMARY KEY,
-    user_id VARCHAR(256),
-    counter_party_id VARCHAR(256),
+    user_id VARCHAR(256) NOT NULL,
+    counter_party_id VARCHAR(256) NOT NULL,
     counter_party_name VARCHAR(256),
-    type VARCHAR(256),
-    status VARCHAR(256),
+    type VARCHAR(20) NOT NULL,
+    status VARCHAR(20) NOT NULL,
     description VARCHAR(256),
-    amount BIGINT,
-    currency VARCHAR(256),
-    updatedBalance BIGINT,
-    dateTime BIGINT
+    amount BIGINT NOT NULL,
+    currency VARCHAR(20) NOT NULL,
+    updated_balance BIGINT NOT NULL,
+    date_time TIMESTAMP NOT NULL
 	);
 
 CREATE TABLE user_info (
 	id VARCHAR(255) PRIMARY KEY,
-	name VARCHAR(255),
-	email_id VARCHAR(255)
+	name VARCHAR(255) NOT NULL,
+	email_id VARCHAR(255) NOT NULL
 );
